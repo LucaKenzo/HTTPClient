a2d2b09ea8c3e68c56af8403a58aad7fa9693021
Luca Kenzo <luca.kenzo0204@gmail.com>
2024-08-29 12:43:30 -0300

    tes2

---
+++ HTTPClient/HTTPClient/Services/PostServices.cs
@@ -0,0 +1,69 @@
+﻿using HTTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Services
+{
+    internal class PostServices
+    {
+        private HttpClient httpClient;
+        private Post post;
+        private ObservableCollection<Post> posts;
+        private JsonSerializerOptions jsonSerializerOptions;
+        Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+
+        public PostServices()
+        {
+            httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<ObservableCollection<Post>> GetPostsAsync()
+        {
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    posts = JsonSerializer.Deserialize<ObservableCollection<Post>>(content, jsonSerializerOptions);
+                }
+            }
+            catch
+            {
+
+            }
+            return posts;
+        }
+
+        public async Task<Post> SavePostAsync(Post item)
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize<Post>(item, jsonSerializerOptions);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await httpClient.PatchAsync(uri, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(response.Content);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            return post;
+        }
+    }
+}
---
+++ HTTPClient/HTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HTTPClient.Models;
+using HTTPClient.Services;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HTTPClient.ViewModels
+{
+    internal class PostViewModel
+    {
+    }
+}
